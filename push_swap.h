abe0c68905b0364804627812749a6b126b3016f7
Gautier Ducurtil <gaducurt@z4r8p6.42lyon.fr>
2025-01-21 16:47:58 +0100

    def struct

--- push_swap.h
+++ push_swap.h
@@ -10,4 +10,17 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
 
+# include <unistd.h>
+# include <stdlib.h>
+
+typedef	struct s_list
+{
+	char			*content;
+	struct s_list	*prev;
+	struct s_list	*next;
+}					t_list;
+
+#endif
